63e5c281029989ebb212f808d196e79263dc42c4
ja74d <pournosrat.javad@gmail.com>
2023-05-10 17:53:00 +0330

    init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main(){
+    float F = 0, Celcus;
+    std::cout << "Enter the temperature:";
+    std::cin >> F;
+    Celcus = ((F-32)*5)/9;
+    std::cout << "in Celcus= " << Celcus << std::endl;
+    return 0;
+}
